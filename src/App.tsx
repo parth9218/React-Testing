96022be7a0212a7571e40c13cb61ffbc0e93642c
Parthkumar Prajapati <parthkumar.prajapati@aircanada.ca>
2024-10-22 03:00:25 -0400

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	new file:   src/components/UserForm/UserForm.test.tsx 	new file:   src/components/UserForm/UserForm.tsx 	new file:   src/components/UserList/UserList.test.tsx 	new file:   src/components/UserList/UserList.tsx 	new file:   src/models/user.model.ts 	modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import { UserForm } from './components/UserForm/UserForm';
+import { UserList } from './components/UserList/UserList';
+import { User } from './models/user.model';
 
 export default function App() {
-  return <></>;
+  const [users, setUsers] = useState<User[]>([]);
+  const handleUserAdd = (user: User) => {
+    setUsers([...users, user]);
+  };
+  return (
+    <div className="App">
+      <UserForm handleUserAdd={handleUserAdd} />
+      <UserList users={users} />
+    </div>
+  );
 }
---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { User } from '../../models/user.model';
+import { validate } from 'class-validator';
+
+interface Props {
+  handleUserAdd: (user: User) => void;
+}
+
+export const UserForm = ({ handleUserAdd }: Props) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const user = new User(name.trim(), email.trim());
+    const validations = await validate(user);
+    if (validations.length > 0) {
+      let errorString = 'Please fix the following errors:\n\n';
+      for (const validation of validations) {
+        for (const key in validation.constraints) {
+          errorString += validation.constraints[key] + '\n';
+        }
+        errorString += '\n';
+      }
+      alert(errorString);
+      return;
+    }
+    handleUserAdd(new User(name, email));
+    clearInputs();
+  };
+
+  const clearInputs = () => {
+    setName('');
+    setEmail('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
