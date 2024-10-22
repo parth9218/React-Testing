502735ca0f4d7f34836cac141bf433aa6b90f2ba
Parthkumar Prajapati <parthkumar.prajapati@aircanada.ca>
2024-10-22 02:35:17 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import "./App.css";
-import ProductList from "./components/ProductList";
+import './App.css';
 
 export default function App() {
-  return (
-    <div className="container mx-auto">
-      <ProductList />
-    </div>
-  );
+  return <></>;
 }
--- src/models/product.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-      id: string;
-      name: string;
-      price: string;
-      material: string;
-      description: string;
-      color: string;
-}
